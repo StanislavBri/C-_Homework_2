8c43e93111264aa7e2f660c43a2664c30a20acb2
Stanislav Brigadin <brigadins@gmail.com>
2023-02-01 20:26:22 +0300

    Add zadacha 10, 13, 15

---
+++ Zadacha_13/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//  645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Random rnd = new Random();
+int a = rnd.Next();
+//int a = rnd.Next(0, 99);
+
+Console.WriteLine(a);
+int ostatok = 0;
+if (Math.Abs(a) > 99) 
+{
+    while (a > 1000)
+    {
+        a = Math.Abs(a) / 10;
+    }
+    ostatok = a % 10;
+    Console.WriteLine(ostatok);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
---
+++ Zadacha_15/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите число дня недели от 1 до 7");
+bool isnumber = int.TryParse(Console.ReadLine(), out int a);
+
+if ((isnumber) && (a < 8) && (a > 1))
+{
+    if ((a == 6) || (a == 7))
+    {
+        Console.WriteLine("да");
+    }
+    else 
+    {
+        Console.WriteLine("нет");
+    }
+}
+else
+{
+    Console.WriteLine("Некорректный ввод");
+}
